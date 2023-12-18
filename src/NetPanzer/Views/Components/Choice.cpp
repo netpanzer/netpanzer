6a2764567b28e795980e45da17493300a05cc7b4
winrid <winrid@gmail.com>
2023-12-17 23:32:31 -0800

    typo (build broken on posix systems)

--- src/NetPanzer/Views/Components/Choice.cpp
+++ src/NetPanzer/Views/Components/Choice.cpp
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Views/Components/View.hpp"
 #include "Views/Components/StateChangedCallback.hpp"
 #include "ViewGlobals.hpp"
-#include "CLasses/ScreenSurface.hpp"
+#include "Classes/ScreenSurface.hpp"
 
 //---------------------------------------------------------------------------
 void Choice::reset()
