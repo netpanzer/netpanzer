9ba0b2b4b76c6a77df451d75340e794a334a9862
andy5995 <arch_stanton5995@protonmail.com>
2024-01-28 03:39:37 -0600

    nls: Mark more strings and add translations

--- src/NetPanzer/Views/Game/HelpScrollViewAlt.cpp
+++ src/NetPanzer/Views/Game/HelpScrollViewAlt.cpp
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //---------------------------------------------------------------------------
 HelpScrollViewAlt::HelpScrollViewAlt() : View() {
   setSearchName("HelpScrollViewAlt");
-  setTitle("Help Information");
+  setTitle(_("Help Information"));
   setSubTitle("");
 
   setAllowResize(false);
