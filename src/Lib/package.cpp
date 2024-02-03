2ecfe9e91c295985a447a6b07602cd0c57342a92
andy5995 <arch_stanton5995@protonmail.com>
2024-02-03 04:13:22 -0600

    typecast so test will build on windows

--- src/Lib/package.cpp
+++ src/Lib/package.cpp
@@ -108,7 +108,7 @@ void test_datadir(void) {
     tmp);
   char expect_data[strlen(tmp) + sizeof "/data"];
   snprintf(expect_data, sizeof expect_data, "%s/data", tmp);
-  assert(Package::getDataDir() == std::filesystem::path(expect_data));
+  assert(strcmp(std::string(Package::getDataDir()).c_str(), expect_data) == 0);
 
   return;
 }
