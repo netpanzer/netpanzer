1c4487899d93d528cac2f59168705894b95ab0cb
andy5995 <arch_stanton5995@protonmail.com>
2024-02-03 23:39:12 -0600

    try to fix conversion error

--- src/Lib/package.cpp
+++ src/Lib/package.cpp
@@ -108,7 +108,7 @@ void test_datadir(void) {
     tmp);
   char expect_data[strlen(tmp) + sizeof "/data"];
   snprintf(expect_data, sizeof expect_data, "%s/data", tmp);
-  assert(strcmp(Package::getDataDir().c_str(), expect_data) == 0);
+  assert(strcmp(Package::getDataDir().string().c_str(), expect_data) == 0);
 
   return;
 }
