28cc7e1d0627e9c099332af5e5635bb09d93615f
andy5995 <arch_stanton5995@protonmail.com>
2024-02-06 05:54:20 -0600

    explicityly add sdl2main to netpanzer_dep (again)

--- setup-build.sh
+++ setup-build.sh
@@ -2,10 +2,7 @@
 
 BUILDDIR=$1
 set -ev
-meson setup "${@}"
-
-# I think I set these properly in the meson build now
-# -Dfreetype2:png=disabled -Dfreetype2:zlib=disabled
+meson setup "${@}" -Dfreetype2:png=disabled -Dfreetype2:zlib=disabled
 
 set +v
 echo Now cd to $BUILDDIR and run ninja
