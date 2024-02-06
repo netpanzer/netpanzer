366ab85159a5bbfd05d36dfe25e324fe5857e78e
andy5995 <arch_stanton5995@protonmail.com>
2024-02-06 04:40:53 -0600

    remove sdl2main from default deps, set default option with_main for sdl2
    and also set zlib and png defaults in meson.build

--- setup-build.sh
+++ setup-build.sh
@@ -2,7 +2,10 @@
 
 BUILDDIR=$1
 set -ev
-meson setup "${@}" -Dfreetype2:png=disabled -Dfreetype2:zlib=disabled
+meson setup "${@}"
+
+# I think I set these properly in the meson build now
+# -Dfreetype2:png=disabled -Dfreetype2:zlib=disabled
 
 set +v
 echo Now cd to $BUILDDIR and run ninja
