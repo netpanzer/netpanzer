d527e1293af5d6043ceaf14266d908aa82919b00
Andy Alt <andy5995@users.noreply.github.com>
2024-02-12 13:46:01 -0600

    appimage: Use linuxdeploy-build-helper-container (remove focal build-env) (#202)
    * remove focal stuff
    
    * distribute platform builds across multiple runners

--- support/appimage/make-appimage.sh
+++ support/appimage/make-appimage.sh
@@ -1,65 +1,93 @@
-#!/bin/sh
-# To create an AppImage from the CI, only this script is neeed and
-# prep-appimage.sh is not used.
+#!/bin/bash
 
+if [ -z "$DOCKER_BUILD" ]; then
+  echo "This script is intended to be run from a docker container."
+  echo "See the docs at https://github.com/andy5995/linuxdeploy-build-helper-container"
+  exit 1
+fi
+
+# Set bash options:
+# -e: Exit immediately if a command exits with a non-zero status.
+# -v: Print each command to stderr before executing it.
 set -ev
 
-if [ -z "$WORKSPACE" ]; then
-  echo "Workspace needs to be set. If you're buildling locally, use prepimage.sh."
-  echo "If calling from a GitHub workflow, be sure to check and set the WORKSPACE"
-  echo "environmental variable."
+# Set default workspace if not provided
+WORKSPACE=${WORKSPACE:-$(pwd)}
+# Check if the workspace path is absolute
+if [[ "$WORKSPACE" != /* ]]; then
+  echo "The workspace path must be absolute"
   exit 1
 fi
+test -d "$WORKSPACE"
 
-if [ -z "$VERSION" ]; then
-  echo "VERSION is set from prep-appimage.sh or the GitHub workflow."
+# Set default source root if not provided
+SOURCE_ROOT=${SOURCE_ROOT:-$WORKSPACE}
+# Check if the source root path is absolute
+if [[ "$SOURCE_ROOT" != /* ]]; then
+  echo "The source root path must be absolute"
   exit 1
 fi
-export LINUXDEPLOY_OUTPUT_VERSION=$VERSION
+# Verify that you're in the source root
+echo $SOURCE_ROOT
+test -f "$SOURCE_ROOT/src/NetPanzer/Core/main.cpp"
 
-OUTPUT_DIR="$WORKSPACE/support"
-test -d "$OUTPUT_DIR"
-APPDIR="$OUTPUT_DIR/AppDir"
-BUILD_DIR="$WORKSPACE/support/docker_build"
+# Define and create application directory if it doesn't exist
+# This is the directory where your project will be installed to
+# and an AppImage created
+APPDIR=${APPDIR:-"/tmp/$USER-AppDir"}
+if [ -d "$APPDIR" ]; then
+  rm -rf "$APPDIR"
+else
+  mkdir -v -p "$APPDIR"
+fi
 
-cd "$WORKSPACE"
+# Install necessary dependencies
+sudo apt install -y \
+  gettext \
+  liblua5.1-0-dev \
+  libphysfs-dev \
+  libsdl2-dev \
+  libsdl2-mixer-dev \
+  libsdl2-ttf-dev
 
-if [ "$CLEAN_BUILD" = "true" ]; then
+# Set up build directory
+BUILD_DIR="$SOURCE_ROOT/appimage_build"
+cd "$SOURCE_ROOT"
+BUILD_DIR="$SOURCE_ROOT/_build_prep_appdir"
+# Clean build directory if specified and it exists
+if [ "$CLEAN_BUILD" = "true" ] && [ -d "$BUILD_DIR" ]; then
   rm -rf "$BUILD_DIR"
 fi
 
-SETUP_CMD=""
-if [ ! -d $BUILD_DIR ]; then
-  SETUP_CMD="./setup-build.sh $BUILD_DIR"
-else
-  cd $BUILD_DIR
-  SETUP_CMD="meson configure"
+# Setup project for building
+if [ ! -d "$BUILD_DIR" ]; then
+  meson setup "$BUILD_DIR" \
+    -Dbuildtype=release \
+    -Dstrip=true \
+    -Db_sanitize=none \
+    -Dprefix=/usr
 fi
 
-$SETUP_CMD \
-  -Dbuildtype=release \
-  -Dstrip=true \
-  -Db_sanitize=none \
-  -Dprefix=/usr
-
-cd $BUILD_DIR
-
-if [ -d "$APPDIR" ]; then
-  rm -rf "$APPDIR"
-fi
+# Build project
+cd "$BUILD_DIR"
 ninja
-meson install --destdir $APPDIR --skip-subprojects
+meson install --destdir=$APPDIR --skip-subprojects
 
-cd "$OUTPUT_DIR"
+# Set up output directory
+OUT_DIR="$WORKSPACE/out"
+if [ ! -d "$OUT_DIR" ]; then
+  mkdir "$OUT_DIR"
+fi
+cd "$OUT_DIR"
 
-# This should set the name of the output appimage but it doesn't
-export NAME="netpanzer"
-linuxdeploy  \
-  -d "$WORKSPACE/support/win32/netpanzer.desktop" \
-  --custom-apprun=$WORKSPACE/support/appimage/AppRun \
-  --icon-file="$WORKSPACE/netpanzer.png" \
+# Set LinuxDeploy output version
+LINUXDEPLOY_OUTPUT_VERSION="$VERSION"
+# Generate AppImage using linuxdeploy
+linuxdeploy \
+  --appdir=$APPDIR \
+  -d $SOURCE_ROOT/support/win32/netpanzer.desktop \
+  --custom-apprun=$SOURCE_ROOT/support/appimage/AppRun \
+  --icon-file=$SOURCE_ROOT/netpanzer.png \
   --icon-filename=netpanzer \
-  --executable "$APPDIR/usr/bin/netpanzer" \
-  --appdir "$APPDIR" \
+  --executable $APPDIR/usr/bin/netpanzer \
   --output appimage
-sha256sum NetPanzer-${VERSION}-${ARCH}.AppImage > NetPanzer-${VERSION}-${ARCH}.AppImage.sha256sum
