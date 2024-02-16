200ab1a9ef91c074504ec10c4c2d9623289222c7
andy5995 <arch_stanton5995@protonmail.com>
2024-02-16 11:43:31 -0600

    meson: Write the correct path to the data in config.h

---
+++ tests/test_data_install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Test for installed data
+
+# As of now, this can be run manually or in the CI, not by 'meson test'.
+# The data must be installed to get an accurate test. See support/Notes.md for
+# details about installing data for testing.
+
+set -ev
+
+SDL_VIDEODRIVER=dummy SDL_AUDIODRIVER=dummy ./netpanzer &
+./netpanzer &
+sleep 10s
+
+if pgrep netpanzer > /dev/null; then
+  pkill netpanzer
+  exit $?
+else
+  exit 1
+fi
+
