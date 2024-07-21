1ce4d97580d1cd7ea67e7aab909aeb3aa7e7aed9
winrid <winrid@gmail.com>
2024-07-21 11:22:21 -0700

    variable speed scrolling by holding down mouse

--- src/NetPanzer/Views/Components/ScrollableText.cpp
+++ src/NetPanzer/Views/Components/ScrollableText.cpp
@@ -22,18 +22,51 @@ void ScrollableText::draw(Surface &dest) {
   maxY = dest.bltStringWrapped(0, offsetY * -1, text.c_str(), color, blend_color, rect.getSizeX());
   upButton->draw(surface);
   downButton->draw(surface);
+  scroll();
+}
+
+void ScrollableText::setScrolling(bool isScrolling, int8_t newDirection) {
+  if (!isScrolling) {
+    scrollStartTime = 0;
+    return;
+  }
+  scrollStartTime = SDL_GetTicks64();
+  direction = newDirection;
+};
+
+void ScrollableText::scroll() {
+  if (!scrollStartTime) {
+    return;
+  }
+  int amount = 20;
+  const uint64_t delta = SDL_GetTicks64() - scrollStartTime;
+  if (delta > 200) {
+    amount = 35;
+  }
+  if (direction < 0) {
+    amount = amount * direction;
+  }
+  if (direction > 0) {
+    if (offsetY < maxY) {
+      offsetY += amount;
+    }
+  } else {
+    if (offsetY > 0) {
+      offsetY += amount;
+    }
+  }
 }
 
 void ScrollableText::actionPerformed(const mMouseEvent &me) {
   if (me.getID() == mMouseEvent::MOUSE_EVENT_PRESSED) {
     if (me.getSource() == upButton) {
-      if (offsetY > 0) {
-        offsetY -= 20;
-      }
+      setScrolling(true, -1);
     } else if (me.getSource() == downButton) {
-      if (offsetY < maxY) {
-        offsetY += 20;
-      }
+      setScrolling(true, 1);
+    } else {
+      setScrolling(false, 0);
     }
+  } else if (me.getID() == mMouseEvent::MOUSE_EVENT_RELEASED) {
+    setScrolling(false, 0);
   }
 }
--- src/NetPanzer/Views/Components/ScrollableText.hpp
+++ src/NetPanzer/Views/Components/ScrollableText.hpp
@@ -23,52 +23,57 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 class ScrollableText : public Component {
 public:
-    ScrollableText(View* view, std::string input_text, iRect input_rect, const PIX input_color, const PIX input_blend_color) : Component(), offsetY(0) {
-      position.zero();
-      text = input_text;
-      rect = input_rect;
-      color = input_color;
-      blend_color = input_blend_color;
-      offsetY = 0;
-      iXY buttonSize(20, 20);
-      iXY buttonPos(input_rect.getSize().x - buttonSize.x, 0);
+  ScrollableText(View *view, std::string input_text, iRect input_rect, const PIX input_color,
+                 const PIX input_blend_color) : Component(), offsetY(0) {
+    position.zero();
+    text = input_text;
+    rect = input_rect;
+    color = input_color;
+    blend_color = input_blend_color;
+    offsetY = 0;
+    setScrolling(false, 0);
+    iXY buttonSize(20, 20);
+    iXY buttonPos(input_rect.getSize().x - buttonSize.x, 0);
 
-      upButton = new Button("upButton");
-      upButton->setLabel("+");
-      upButton->setLocation(buttonPos.x, buttonPos.y);
-      upButton->setSize(buttonSize.x, buttonSize.y);
-      upButton->setNormalBorder();
-      upButton->setTextColors(Color::darkGray, Color::red, Color::gray);
-      view->add(upButton); // TODO support component hierarchy in View.cpp mouseMove()
+    upButton = new Button("upButton");
+    upButton->setLabel("+");
+    upButton->setLocation(buttonPos.x, buttonPos.y);
+    upButton->setSize(buttonSize.x, buttonSize.y);
+    upButton->setNormalBorder();
+    upButton->setTextColors(Color::darkGray, Color::red, Color::gray);
+    view->add(upButton); // TODO support component hierarchy in View.cpp mouseMove()
 
-      buttonPos = iXY(input_rect.getSize().x - buttonSize.x,
-                      input_rect.getSize().y - buttonSize.y);
-      downButton = new Button("downButton");
-      downButton->setLabel("-");
-      downButton->setLocation(buttonPos.x, buttonPos.y);
-      downButton->setSize(buttonSize.x, buttonSize.y);
-      downButton->setNormalBorder();
-      downButton->setTextColors(Color::darkGray, Color::red, Color::gray);
-      view->add(downButton); // TODO support component hierarchy in View.cpp mouseMove()
-    }
+    buttonPos = iXY(input_rect.getSize().x - buttonSize.x,
+                    input_rect.getSize().y - buttonSize.y);
+    downButton = new Button("downButton");
+    downButton->setLabel("-");
+    downButton->setLocation(buttonPos.x, buttonPos.y);
+    downButton->setSize(buttonSize.x, buttonSize.y);
+    downButton->setNormalBorder();
+    downButton->setTextColors(Color::darkGray, Color::red, Color::gray);
+    view->add(downButton); // TODO support component hierarchy in View.cpp mouseMove()
+  }
 
 protected:
-    int offsetY;
-    int maxY;
-    std::string text;
-    iRect rect;
-    PIX color;
-    PIX blend_color;
+  int offsetY;
+  int maxY;
+  std::string text;
+  iRect rect;
+  PIX color;
+  PIX blend_color;
+  int8_t direction;
+  // 64bit just encase someone leaves the game open for months :)
+  uint64_t scrollStartTime;
 
-    Button *upButton;
-    Button *downButton;
+  Button *upButton;
+  Button *downButton;
 
 public:
-    virtual void actionPerformed(const mMouseEvent &me);
-
-    void draw(Surface &dest);
-
-    virtual void render() {}
+  virtual void actionPerformed(const mMouseEvent &me);
+  void draw(Surface &dest);
+  virtual void render() {};
+  void setScrolling(bool isScrolling, int8_t newDirection);
+  void scroll();
 };
 
 
