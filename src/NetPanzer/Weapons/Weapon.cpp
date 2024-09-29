9b3fa62282b04af8d5d40790bff14e2761c415f0
winrid <winrid@gmail.com>
2024-09-29 15:08:56 -0700

    fixing segfault on server

--- src/NetPanzer/Weapons/Weapon.cpp
+++ src/NetPanzer/Weapons/Weapon.cpp
@@ -113,12 +113,14 @@ void Weapon::fsmFlight(void) {
         fsmFlight_state = _fsmFlight_idle;
         lifecycle_status = _lifecycle_weapon_in_active;
 
-        // SFX
-        sound->playAmbientSound("hit_target", WorldViewInterface::getCameraDistance(location));
+        if (NetworkState::status != _network_state_server) {
+          // SFX
+          sound->playAmbientSound("hit_target", WorldViewInterface::getCameraDistance(location));
 
-        // **  Particle Shit
-        iXY loc = iXY(location.x, location.y);
-        ParticleInterface::addMiss(loc, Weapon::owner_type_id);
+          // Particles
+          iXY loc = iXY(location.x, location.y);
+          ParticleInterface::addMiss(loc, Weapon::owner_type_id);
+        }
         end_cycle = true;
         break;
       }
