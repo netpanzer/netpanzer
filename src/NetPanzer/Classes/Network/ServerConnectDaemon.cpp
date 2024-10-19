beb0c252a1259a43e0e22ebdd205e5b235924d81
andy5995 <arch_stanton5995@proton.me>
2024-10-19 17:49:26 -0500

    CWE-457: Use of Uninitialized Variable
    https://scan9.scan.coverity.com/#/project-view/60713/15801?selectedIssue=425076

--- src/NetPanzer/Classes/Network/ServerConnectDaemon.cpp
+++ src/NetPanzer/Classes/Network/ServerConnectDaemon.cpp
@@ -607,6 +607,7 @@ class StateUnitSync : public State<T> {
     sync_end += 1;
 
     ConnectProcessStateMessage state_mesg;
+    state_mesg.percent_complete = 0;
     state_mesg.setMessageEnum(_connect_state_message_sync_units);
     connect_client->sendMessage(&state_mesg,
                                 sizeof(ConnectProcessStateMessage));
